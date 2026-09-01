605dd48b1cad5ed965df3faa26ce5f2e6feee5be
kantarokotasan-a11y <kantaro.kotasan@gmail.com>
2026-09-01 16:08:34 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-const CACHE='sushi-app-v3';
-const ASSETS=['./','./index.html','./manifest.json',
-  './icon-192.png','./icon-512.png','./icon-180.png','./icon-maskable-512.png'];
+const CACHE='sushi-app-v4';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-180.png','./icon-maskable-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
